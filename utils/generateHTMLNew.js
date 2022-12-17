7bcbe4b7fe70bed8bad38207f096b24b99f94738
gato365 <immanuelwilliams@gato365.com5>
2022-12-17 05:01:47 -0800

    Made generateHTML function work

--- utils/generateHTMLNew.js
+++ utils/generateHTMLNew.js
@@ -4,26 +4,35 @@ function generateHTML(currentManager, currentEngineer, currentIntern) {
 
     const teamInfo = currentManager.getTeamInfo();
 
+
+    let managerPic;
+    let engineerPic;
+    let internPic;
+    let decription;
+
+
+
+
     if (teamInfo === "The Alliance") {
-        managePic = "vince-mcmahon.jpg";
+        managerPic = "vince-mcmahon.jpg";
         engineerPic = "stephanie-mcmahon.jpg";
         internPic = "shane-mcmahon.jpg";
         decription = "The Alliance, also known as Team WCW/ECW and The Coalition, was a professional wrestling stable in the World Wrestling Federation (WWF, now WWE) that existed during the Invasion storyline from July to November 2001.";
 
-    } else if (teamInfo === " Rock 'n' Sock") {
-        managePic = "the-rock.jpg";
+    } else if (teamInfo === "Rock 'n' Sock") {
+        managerPic = "the-rock.jpg";
         engineerPic = "mike-foley.jpg";
         internPic = "mr-socko.jpg";
         decription = "The Rock 'n' Sock Connection was a professional wrestling tag team of The Rock and Mankind/Mick Foley who wrestled in the World Wrestling Federation between 1999 and 2000 and briefly in 2004. As a team, they held the WWF Tag Team Championship three times";
     } else if (teamInfo === "D-Generation X") {
-        managePic = "chyna.jpg";
+        managerPic = "chyna.jpg";
         engineerPic = "triple-h.jpg";
         internPic = "xpac.jpg";
         decription = "The group originated in the World Wrestling Federation (WWF, now known as WWE) in the midst of the \"Attitude Era\" in 1997 as a foil to another prominent faction, The Hart Foundation and became one of the main driving forces behind the WWF competing with World Championship Wrestling (WCW) in the Monday Night Wars. In addition to two other founding members Chyna and Rick Rude aside from Michaels and Triple H, the group expanded with new additions X-Pac, The New Age Outlaws (Road Dogg and Billy Gunn), and Tori until it disbanded in August 2000. ";
     }
 
 
-   
+
 
     const htmlFile = `
 <!DOCTYPE html>
@@ -35,6 +44,7 @@ function generateHTML(currentManager, currentEngineer, currentIntern) {
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.3.1/dist/css/bootstrap.min.css"
         integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
 
+        
     <title>${currentManager.getTeamInfo()} Page </title>
     <style>
         .card-img-top {
@@ -84,7 +94,7 @@ function generateHTML(currentManager, currentEngineer, currentIntern) {
         <div class="d-flex justify-content-around align-items-center" >
 
             <div class="card" style="width: 35rem;" id="manager">
-                <img class="card-img-top" src="./images/chyna.jpg" alt="Card image cap">
+                <img class="card-img-top" src="./images/${managerPic}" alt="Card image cap">
                 <div class="card-body">
                     <h5 class="card-title text-info">
                         <p> <b>Role:</b> Manager</p>
@@ -112,7 +122,7 @@ function generateHTML(currentManager, currentEngineer, currentIntern) {
 
 
             <div class="card" style="width: 35rem;" id="engineer">
-                <img class="card-img-top" src="./images/triple-h.jpg" alt="Card image cap">
+                <img class="card-img-top" src="./images/${engineerPic}" alt="Card image cap">
                 <div class="card-body">
                     <h5 class="card-title text-info">
                         <p> <b>Role:</b> Engineer</p>
@@ -140,7 +150,7 @@ function generateHTML(currentManager, currentEngineer, currentIntern) {
 
 
             <div class="card" style="width: 35rem;" id="intern">
-                <img class="card-img-top" src="./images/${+ internPic}.jpg" alt="Card image cap">
+                <img class="card-img-top" src="./images/${internPic}" alt="Card image cap">
                 <div class="card-body">
                     <h5 class="card-title text-info">
                         <p> <b>Role:</b> Intern</p>
