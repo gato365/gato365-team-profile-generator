57f74ba9160b4c0cf8c39b2ee976253e9c71ce5d
gato365 <immanuelwilliams@gato365.com5>
2022-12-17 05:52:00 -0800

    Got code to a place in which it works

--- index.js
+++ index.js
@@ -1,65 +1,6 @@
 
 
-
-
-const Input = require("./lib/Input");
-
-// -----------------Function Definitions--------------------
-// Author: Immanuel Williams PhD 
-// Date Created: (FLEX Staff)
-// Date Modified: 12/16/2022
-// Name: writeToFile
-// Purpose: runs questions of prompt and stores information into data then runs writeToFile
-// Input: NA
-// Output: NA
-// Notes: NA
-// -----------------Function Definitions--------------------
-function init() {
-    inquirer.prompt(questionsManager).then((dataManager) => {
-        inquirer.prompt(questionsEngineer).then((dataEngineer) => {
-            inquirer.prompt(questionsIntern).then((dataIntern) => {
-
-
-                // Create a new Manager
-                const currentManager = new Manager(
-                    dataManager.employeeManagerName,
-                    dataManager.employeeManagerID,
-                    dataManager.employeeManagerEmail,
-                    dataManager.officeNumber,
-                    dataManager.teamInfo,
-                    );
-
-
-                // Create a new Engineer
-                const currentEngineer = new Engineer(
-                    dataEngineer.employeeEngineerName,
-                    dataEngineer.employeeEngineerID,
-                    dataEngineer.employeeEngineerEmail,
-                    dataEngineer.githubAccount);
-
-
-
-                // Create a new Intern
-                const currentIntern = new Intern(
-                    dataIntern.employeeInternName,
-                    dataIntern.employeeInternID,
-                    dataIntern.employeeInternEmail,
-                    dataIntern.schoolName);
-
-
-                    // Display Information
-                    // testConsoleLogInfo()
-                //    const oldhtmlFile = testHTML(currentManager,currentEngineer,currentIntern);
-                   const newhtmlFile = generateHTML(currentManager,currentEngineer,currentIntern);
-
-                   writeToFile('index.html', newhtmlFile);
-
-                    
-            });
-
-        });
-    });
-}
+const Input = require("./lib/input");
 
 // Function call to initialize app
 init();
